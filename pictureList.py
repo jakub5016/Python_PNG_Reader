c36ccc3bc339f5a232daad6982ceac5c70f5f667
jakub5016 <me.jakub.gil@gmail.com>
2024-03-14 11:50:47 +0100

    Row split works but for string

--- pictureList.py
+++ pictureList.py
@@ -1,6 +1,55 @@
 from hexFunctions import delete_spaces_from_hex, add_spaces_to_hex
 
+PNG_SIGNATURE= "89 50 4E 47 0D 0A 1A 0A"
+PNG_SIGNATURE_NO_SPACE = "89504E470D0A1A0A"
+LENGTH_SIZE = 4*2
+CHUNK_TYPE_SIZE = 4*2
+CRC_SIZE = LENGTH_SIZE
+
 class PictueList(list):
+    def __init__(self, hex_no_space):
+        cursor = len(PNG_SIGNATURE_NO_SPACE)
+    
+        while(cursor < len(hex_no_space)):
+            subclass_arr =[] # This array contains info about every chunk in picture
+            length = int(hex_no_space[cursor:cursor+LENGTH_SIZE], 16)
+            cursor+=LENGTH_SIZE
+            subclass_arr.append(length)
+            
+            chunk_type_hex = hex_no_space[cursor:cursor+CHUNK_TYPE_SIZE]
+            byte_string = bytes.fromhex(chunk_type_hex)  
+            chunk_type = byte_string.decode("ASCII")  
+            subclass_arr.append(chunk_type)
+            cursor+=CHUNK_TYPE_SIZE
+            if ((chunk_type == "IEND") and (hex_no_space[cursor:cursor+CRC_SIZE] == "AE426082")):
+                print("Valid IEND appeared, EOF\n")
+                subclass_arr.append(None)
+                subclass_arr.append(None)
+                self.append(subclass_arr)
+                return(None)
+            
+            elif (chunk_type == "IEND"):
+                print("Invalid IEND appeared, file might be broken")
+                return(None)
+
+            content = hex_no_space[cursor:cursor+(length*2)] # Byte = 2x hex 
+            content_with_spaces = add_spaces_to_hex(content)
+            cursor+=length*2
+            subclass_arr.append(content_with_spaces)
+
+            crc = hex_no_space[cursor:cursor+CRC_SIZE]
+            crc_with_spaces = add_spaces_to_hex(crc)
+            cursor+=CRC_SIZE
+            subclass_arr.append(crc_with_spaces)
+
+            self.append(subclass_arr)
+
+    def mout(self):
+        self._get_info_form_IHDR()
+        self.read_palette()
+        self.generate_pixels()
+
+
     def print_chunk_types(self):    
         for i in self:
             print(i[1])
@@ -24,15 +73,20 @@ class PictueList(list):
         print(f"Filter Method: {self.filter_method}")
         print(f"Interlace Method: {self.interlace_method}")
     
+    def get_chunk_index(self, chunk_name):
+        chunk_index = 0
+        for index, i in enumerate(self):
+            if i[1] == chunk_name:
+                chunk_index = index
+        return chunk_index
+    
+
     def read_palette(self):
         if self.color_type != 3:
             print("This picture doesn't contain color palette")
             return(-1)
 
-        palette_index = 0
-        for index, i in enumerate(self):
-            if i[1] == "PLTE":
-                palette_index = index
+        palette_index = self.get_chunk_index("PLTE")
 
         palette_date = delete_spaces_from_hex(self[palette_index][2]) 
         self.palette = []
@@ -41,10 +95,10 @@ class PictueList(list):
                                  int(palette_date[i+1], 16), 
                                  int(palette_date[i+2], 16)])
     def generate_pixels(self):
-        pixels = []
-        IHDR_data = delete_spaces_from_hex(self[0][2])
+        self.pixels = []
 
-        for index in range(0 ,len(IHDR_data), self.width):
-            pixels.append(IHDR_data[index:index+self.width])
 
-        print(index)
+        IDAT_data = self[self.get_chunk_index("IDAT")][2]
+        print(IDAT_data)
+        for index in range(0 ,len(IDAT_data), self.width):
+            self.pixels.append(IDAT_data[index:index+self.width])
--- read.py
+++ read.py
@@ -10,20 +10,19 @@ CRC_SIZE = LENGTH_SIZE
 # Byte = 2x hex 
 
 
-
 if __name__ == "__main__":
-   if len(sys.argv) != 2 :
-       print("Too many or less arguments"+ len(sys.argv))
-       raise(AttributeError)
+    if len(sys.argv) != 2 :
+        print("Too many or less arguments"+ len(sys.argv))
+        raise(AttributeError)
    
-   png_file_path = sys.argv[1]
+    png_file_path = sys.argv[1]
 
     #### READ DATA
-   with open(png_file_path, 'rb') as binary_file:
-    binary_data = binary_file.read()
+    with open(png_file_path, 'rb') as binary_file:
+        binary_data = binary_file.read()
 
-    hex_representation = ' '.join(f'{byte:02X}' for byte in binary_data)
-    hex_no_space = delete_spaces_from_hex(hex_representation)
+        hex_representation = ' '.join(f'{byte:02X}' for byte in binary_data)
+        hex_no_space = delete_spaces_from_hex(hex_representation)
 
     #### CHECK IF PNG
     if(hex_no_space[0:len(PNG_SIGNATURE_NO_SPACE)] != PNG_SIGNATURE_NO_SPACE):
@@ -34,44 +33,8 @@ if __name__ == "__main__":
 
     #### START CURSOR READING
     cursor = len(PNG_SIGNATURE_NO_SPACE)
-    picture_arr=PictueList()
-    
-    while(cursor < len(hex_no_space)):
-        subclass_arr =[] # This array contains info about every chunk in picture
-        length = int(hex_no_space[cursor:cursor+LENGTH_SIZE], 16)
-        cursor+=LENGTH_SIZE
-        subclass_arr.append(length)
-        
-        chunk_type_hex = hex_no_space[cursor:cursor+CHUNK_TYPE_SIZE]
-        byte_string = bytes.fromhex(chunk_type_hex)  
-        chunk_type = byte_string.decode("ASCII")  
-        subclass_arr.append(chunk_type)
-        cursor+=CHUNK_TYPE_SIZE
-        if ((chunk_type == "IEND") and (hex_no_space[cursor:cursor+CRC_SIZE] == "AE426082")):
-            print("Valid IEND appeared, EOF\n")
-            subclass_arr.append(None)
-            subclass_arr.append(None)
-            picture_arr.append(subclass_arr)
-            picture_arr._get_info_form_IHDR()
-            picture_arr.print_IDHR_INFO()
-            picture_arr.read_palette()
-            print(picture_arr.palette)
-            exit(0)
-        
-        elif (chunk_type == "IEND"):
-            print("Invalid IEND appeared, file might be broken")
-            exit(-1)
-
-        content = hex_no_space[cursor:cursor+(length*2)] # Byte = 2x hex 
-        content_with_spaces = add_spaces_to_hex(content)
-        cursor+=length*2
-        subclass_arr.append(content_with_spaces)
-
-        crc = hex_no_space[cursor:cursor+CRC_SIZE]
-        crc_with_spaces = add_spaces_to_hex(crc)
-        cursor+=CRC_SIZE
-        subclass_arr.append(crc_with_spaces)
-
-        picture_arr.append(subclass_arr)
-
+    picture_arr=PictueList(hex_no_space=hex_no_space)
+    picture_arr.mout()
+    picture_arr.print_IDHR_INFO()
+    print(picture_arr.pixels)
     
