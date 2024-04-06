51c5ebec2e62c5611ffaa355aef9edba38aa1a2c
jakub5016 <me.jakub.gil@gmail.com>
2024-04-06 21:51:36 +0200

    Fix

--- part1/src/chroma.py
+++ part1/src/chroma.py
@@ -1,6 +1,9 @@
 from src.hexFunctions import delete_spaces_from_hex
 
 def print_chroma(picture_arr):
+    if picture_arr.get_chunk_index("cHRM") == 0:
+        print("There is no cHRM chunk in this file")
+        return -1
     val = delete_spaces_from_hex(picture_arr[picture_arr.get_chunk_index("cHRM")][2])
     white_point = [int(val[0:8],16)/100000, int(val[8:16],16)/100000]
     red = [int(val[16:24],16)/100000, int(val[24:32],16)/100000]
